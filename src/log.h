f2452cf8f035a0807eb0e1d2ec4eee2fa7b2c890
peitianyu <1216582461@qq.com>
2023-04-24 19:33:56 +0800

    整理代码

--- src/log.h
+++ src/log.h
@@ -1,6 +1,9 @@
 #ifndef __COMMON_LOG_H__
 #define __COMMON_LOG_H__
 
+// #define LOG_TO_FILE
+#ifdef LOG_TO_FILE
+
 #include<fstream>
 #include<sstream>
 #include<sys/stat.h>
@@ -11,41 +14,38 @@
 class LogFile
 {
 public:
-    LogFile()
-    {
-        m_file_name = LOG_FILE_PATH + std::string(__DATE__) + "_" + std::to_string(m_file_num++) + ".log";
-        m_file.open(m_file_name, std::ios::out | std::ios::app);
-    }
+    LogFile();
 
-    ~LogFile() { m_file.close(); }
+    ~LogFile();
 
     template <typename T>
-    void WriteLog(const T &arg){
+    std::ofstream& WriteLog(const T &arg){
         m_file << arg;
+
+        return m_file;
     }
 
     template <typename T, typename... Types>
-    void WriteLog(const T &firstArg, const Types &...args){
+    std::ofstream& WriteLog(const T &firstArg, const Types &...args){
         m_file << firstArg;
-        WriteLog(args...);
+        return WriteLog(args...);
     }
 
-    void Flush() { m_file.flush(); }
+    void Flush();
 
-    std::string GetFileName() { return m_file_name; }
+    std::string GetFileName();
 
-    std::size_t GetFileSize() { return m_file.tellp(); }
+    std::size_t GetFileSize();
 private:
-    static uint m_file_num;
+    static uint m_log_file_num;
     std::string m_file_name;
     std::ofstream m_file;
 };
 
-uint LogFile::m_file_num = 0;
 
-#define MAX_LOG_CACHE_SIZE 1024 // 修改缓存日志大小
-#define MAX_LOG_FILE_SIZE 2048  // 修改单文件最大大小
-#define MAX_LOG_FILE_NUM 10     // 修改最大文件数
+#define MAX_LOG_CACHE_SIZE 1024                         // 修改缓存日志大小
+#define MAX_LOG_FILE_SIZE 1024 * 1024 * 10              // 修改单文件最大大小
+#define MAX_LOG_FILE_NUM 10                             // 修改最大文件数
 
 
 class LogFileManage
@@ -58,77 +58,49 @@ public:
     }
 
     template <typename T>
-    void WriteLog(const T &arg)
+    std::ofstream& WriteLog(const T &arg)
     {
         m_mutex.lock();
         ManageFile();
-
-        m_log_file->WriteLog(arg);
         m_mutex.unlock();
+
+        return m_log_file->WriteLog(arg);
     }
 
     template <typename T, typename... Types>
-    void WriteLog(const T &firstArg, const Types &...args)
+    std::ofstream& WriteLog(const T &firstArg, const Types &...args)
     {
         m_mutex.lock();
         ManageFile();
-
         m_log_file->WriteLog(firstArg);
-        WriteLog(args...);
         m_mutex.unlock();
+
+        return WriteLog(args...);
     }
 
 private:
-    void ManageFile()
-    {
-        if (m_log_file == nullptr){
-            m_log_file = new LogFile();
-        }
+    void ManageFile();
 
-        struct stat statbuf;
-        stat(m_log_file->GetFileName().c_str(), &statbuf);
+    void RemoveFile();
 
-        if (statbuf.st_size > MAX_LOG_FILE_SIZE){
-            delete m_log_file;
-            m_log_file = new LogFile();
-
-            RemoveFile();
-        }
-
-        if (m_log_file->GetFileSize() > MAX_LOG_CACHE_SIZE)
-        {
-            m_log_file->Flush();
-        }
-    }
-
-    void RemoveFile()
-    {
-        // 删除超出最大文件数的文件
-        std::string cmd = "ls " + std::string(LOG_FILE_PATH) + " -t | awk '{if(NR>10) print $0}'";
-        FILE *fp = popen(cmd.c_str(), "r");
-        if (fp == nullptr) return;
-
-        char buf[1024];
-        while (fgets(buf, sizeof(buf), fp) != nullptr){
-            std::string file_name = std::string(LOG_FILE_PATH) + buf;
-            file_name.erase(file_name.end() - 1);
-            remove(file_name.c_str());
-        }
-    }
-
-    LogFileManage() : m_log_file(nullptr) {}
-    ~LogFileManage()
-    {
-        if (m_log_file != nullptr){
-            delete m_log_file;
-            m_log_file = nullptr;
-        }
-    }
+    LogFileManage();
+    ~LogFileManage();
 
 private:
     LogFile *m_log_file;
     std::mutex m_mutex;
 };
+
+#define LOG_FILE_CORE(...) LogFileManage::GetInstance()->WriteLog("[", __TIME__, "] ", __VA_ARGS__)
+
+#define LOG_DEBUG(...) LOG_FILE_CORE("[DEBUG] [", __FUNCTION__, __LINE__, "]", __VA_ARGS__)
+#define LOG_INFO(...) LOG_FILE_CORE(" [INFO] ", __VA_ARGS__)
+#define LOG_WARN(...) LOG_FILE_CORE(" [WARN] [", __FUNCTION__, __LINE__, "]", __VA_ARGS__)
+#define LOG_ERROR(...) LOG_FILE_CORE("[ERROR] [", __FUNCTION__, __LINE__, "]", __VA_ARGS__)
+
+#else
+
+#include<iostream>
 
 template <typename T>
 std::ostream &STD_COUT(std::ostream &os, const T &arg) { return os << arg;}
@@ -136,20 +108,14 @@ std::ostream &STD_COUT(std::ostream &os, const T &arg) { return os << arg;}
 template <typename T, typename... Types>
 std::ostream &STD_COUT(std::ostream &os, const T &firstArg, const Types &...args){ os << firstArg << " "; return STD_COUT(os, args...);}
 
-// #define LOG_TO_FILE
-#ifdef LOG_TO_FILE
-#define LOG_FILE_CORE(...) LogFileManage::GetInstance()->WriteLog("[", __TIME__, "] ", __VA_ARGS__)
-#else
-#define LOG_FILE_CORE(...) STD_COUT(std::cout, "[", __TIME__, "] ", __VA_ARGS__)
+#define LOG_DEBUG(...) STD_COUT(std::cout, "\033[33m[", __TIME__, "] [DEBUG] [", __FUNCTION__, __LINE__, "] : ", __VA_ARGS__, "\033[0m")
+#define LOG_INFO(...) STD_COUT(std::cout, "\033[32m[", __TIME__, "]  [INFO] [", __FUNCTION__, __LINE__, "] : ", __VA_ARGS__, "\033[0m")
+#define LOG_WARN(...) STD_COUT(std::cout, "\033[35m[", __TIME__, "]  [WARN] [", __FUNCTION__, __LINE__, "] : ", __VA_ARGS__, "\033[0m")
+#define LOG_ERROR(...) STD_COUT(std::cout, "\033[31m[", __TIME__, "] [ERROR] [", __FUNCTION__, __LINE__, "] : ", __VA_ARGS__, "\033[0m")
+
 #endif // LOG_TO_FILE
 
-#define LOG_DEBUG(...) LOG_FILE_CORE("[DEBUG] [", __FUNCTION__, __LINE__, "]", __VA_ARGS__)
 
-#define LOG_INFO(...) LOG_FILE_CORE(" [INFO] ", __VA_ARGS__)
-
-#define LOG_WARN(...) LOG_FILE_CORE(" [WARN] [", __FUNCTION__, __LINE__, "]", __VA_ARGS__)
-
-#define LOG_ERROR(...) LOG_FILE_CORE("[ERROR] [", __FUNCTION__, __LINE__, "]", __VA_ARGS__)
 
 
 
--- src/main.cc
+++ src/main.cc
@@ -6,10 +6,10 @@
 void Test()
 {
     for(int i = 0; i < 1e6; i++){
-        LOG_DEBUG("Hello World! ", i, "\n");
-        LOG_INFO("Hello World! ", i, "\n");
-        LOG_WARN("Hello World! ", i, "\n");
-        LOG_ERROR("Hello World! ", i, "\n");
+        LOG_DEBUG("Hello World! ", i) << std::endl;
+        LOG_INFO("Hello World! ", i) << std::endl;
+        LOG_WARN("Hello World! ", i) << std::endl;
+        LOG_ERROR("Hello World! ", i) << std::endl;
 
         usleep(100000);
     }
